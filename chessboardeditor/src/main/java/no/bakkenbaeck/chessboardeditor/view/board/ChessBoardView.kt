7a386a30134dc24de9be704277a1619b4db6d6f6
csdodd <colin@bakkenbaeck.no>
2019-09-24 09:51:31 +0200

    Always init the board
    This means you don't have to call "setFen", be default the view will show an empty board.

--- chessboardeditor/src/main/java/no/bakkenbaeck/chessboardeditor/view/board/ChessBoardView.kt
+++ chessboardeditor/src/main/java/no/bakkenbaeck/chessboardeditor/view/board/ChessBoardView.kt
@@ -26,6 +26,10 @@ class ChessBoardView @JvmOverloads constructor(
 
     private lateinit var position: Position
 
+    init {
+        setFen("")
+    }
+    
     fun setFen(fen: String) {
         try {
             position = FenUtil.readFEN(fen)
